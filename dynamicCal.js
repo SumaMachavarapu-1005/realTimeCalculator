0cd4a75a2b9d67fce239dc38abef9526680a8144
SumaMachavarapu-1005 <99955049+SumaMachavarapu-1005@users.noreply.github.com>
2023-06-16 11:05:04 +0530

    Add files via upload

---
+++ dynamicCal.js
@@ -0,0 +1,52 @@
+var ans=0;
+var s="";
+document.getElementById("add").onclick=function(){
+    var a=parseInt(document.getElementById("n1").value);
+    ans=ans+a;
+    if(s.length==0)
+    s+=a;
+    else
+    s=s+("+"+a);
+    document.getElementById("result").textContent=ans;
+
+}
+document.getElementById("sub").onclick=function(){
+    var a=parseInt(document.getElementById("n1").value); 
+    ans=ans-a;
+    if(s.length==0)
+    s+=a;
+    else
+    s=s+("-"+a);
+    document.getElementById("result").textContent=ans; 
+}
+document.getElementById("mul").onclick=function(){
+    var a=parseInt(document.getElementById("n1").value);
+    ans=ans*a;
+    if(s.length==0)
+    s+=a;
+    else
+    s=s+("*"+a);
+    document.getElementById("result").textContent=ans; 
+}
+document.getElementById("div").onclick=function()
+{
+    var a=parseInt(document.getElementById("n1").value);
+    ans=ans/a;
+    if(s.length==0)
+    s+=a;
+    else
+    s=s+("/"+a);
+    document.getElementById("result").textContent=ans; 
+}
+document.getElementById("mod").onclick=function(){
+    var a=parseInt(document.getElementById("n1").value);
+    ans=ans%a;
+    if(s.length==0)
+    s+=a;
+    else
+    s=s+("%"+a);
+    document.getElementById("result").textContent=ans; 
+}
+equal=()=>{
+    document.getElementById("wait").textContent=s;
+}
